0ef7c14d080b0a98d94b02d382638226b0f838c9
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-02 14:11:47 -0500

    Adding main file

---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+    Creating Class Objects
+"""
+class animals:
+    """
+        General Class
+    """
+    def __init__(self, name="", species="", age=0):
+        self.name = name
+        self.species = species
+        self.age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def setname(self, value):
+        if type(value) is not str:
+            raise TypeError("Name must be a string")
+        self.__name = value
+
+    @property
+    def species(self):
+        return self.__species
+    
+    @species.setter
+    def setspecies(self, value):
+        if type(value) is not str:
+            raise TypeError("Species must be a string")
+        self.__species = value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def setage(self, value):
+        if type(value) is not int:
+            raise TypeError("Age must be an int")
+        if value <= 0:
+            raise ValueError("Age must be bigger than cero")
+        self.__age = value
+
+    def move(self, position=""):
+        if type(position) is not str:
+            raise TypeError("Position must be a string")
+        print(f"Direction: {position}")
+
+    def show(self):
+        print(f"I'm {self.__name}")
+        print(f"My Specie is: {self.__species}")
+        print(f"I'm {self.__age} years old")
+
+    def sound(self, sound=""):
+        if type(sound) is not str:
+            raise TypeError("Sound must be a string")
+        print(f"My sound is: {sound}")
